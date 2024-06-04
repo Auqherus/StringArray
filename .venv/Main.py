4cfb3f504085012f0b9d486f1a128d4be489d9be
Auqherus <patrykromanowski@gmail.com>
2024-06-04 21:25:39 +0200

    Small update

---
+++ .venv/Main.py
@@ -0,0 +1,23 @@
+#1 Excercise
+
+q = ["T", "H" , "E"," ","E","Y","E","S"]
+
+print(q[0], q[1], q[2], q[5] , q[3] , q[7] , q[4] , q[6]) # they see
+
+#2 Exercise
+
+d = ["a"," ", "g", "e", "n", "t", "l","e", "m", "a", "n"]
+print(d[3], d[6], d[7], d[2], d[0], d[4], d[5], d[1], d[8], d[9], d[10] ) # elegant man
+
+#3 Exercise
+
+e = ["T", "H", "E"," ","M","O","R","S", "E", " ", "C", "O", "D", "E"]
+print(e[1], e[2], e[6], e[8], e[3], e[10], e[5], e[4], e[13], e[9], e[12], e[11], e[0], e[7] ) # Here come dots
+
+#4 Exercise
+
+line = '"Kropka nad i" nadamy o: 22:00'
+
+time = line[line.find("o:"):+2:]
+
+print(time)
